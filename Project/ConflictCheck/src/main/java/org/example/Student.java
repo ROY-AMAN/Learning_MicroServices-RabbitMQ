40450d86cafbc7a8b861bc205f99ce90c0cad73c
aman <cs.amanroy@gmail.com>
2023-01-16 13:12:55 +0530

    Aman Bhai Ye Commit kr rhe hai

--- Project/ConflictCheck/src/main/java/org/example/Student.java
+++ Project/ConflictCheck/src/main/java/org/example/Student.java
@@ -3,8 +3,8 @@ package org.example;
 public class Student {
 
     public static void getstudent(){
-        for(int i=0; i<50; i++){
-            System.out.println("Print here thr valuse is "+i);
+        for(int i=10; i<80; i++){
+            System.out.println("Aman ki Value hai ye"+i);
         }
     }
 }
