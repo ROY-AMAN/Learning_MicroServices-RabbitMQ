dcecaabe9bffc6c885f27fcbe673029ca488d7a7
chandan <chandanmourya4786@gmail.com>
2023-01-16 12:29:54 +0530

    added chadan Controller

---
+++ Project/SpringSecurityProject/src/main/java/aman/revlitix/LibraryManagementSystem/Controller/ChandanController.java
@@ -0,0 +1,18 @@
+package aman.revlitix.LibraryManagementSystem.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChandanController {
+
+    @PostMapping("/con")
+    public String usingForLoop(){
+        Integer j=0;
+        for(int i=0; i<=10; i++){
+            j+=i;
+            System.out.println("Printing the Values "+ i);
+        }
+        return "Total Sum is "+j;
+    }
+}
