f015d30b4f0ee51b462aa9e27a873e0c73e90468
Aman Raj <roy044556@gmail.com>
2023-01-16 12:51:59 +0530

    new conflict project added

---
+++ Project/ConflictCheck/src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Student {
+
+    public static void getstudent(){
+        for(int i=0; i<50; i++){
+            System.out.println("Print here thr valuse is "+i);
+        }
+    }
+}
